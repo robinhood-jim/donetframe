d213441e4c0db5cad34e27f844a68e04b773c119
robinjim@126.com <robinjim@126.com>
2026-06-30 19:04:41 +0800

    missing Handler

---
+++ Frameset/Core/Dao/Handler/MetaObjectHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Frameset.Core.Dao.Handler
+{
+    public abstract class MetaObjectHandler
+    {
+        public abstract void InsertFill(MetaObject metaObject);
+        public abstract void UpdateFill(MetaObject metaObject);
+        public void SetValueByName(string fieldName, object value, MetaObject metaObject)
+        {
+            if (value != null && metaObject.HasSetter(fieldName))
+            {
+                metaObject.SetValue(fieldName, value);
+            }
+        }
+        public bool ContainColumn(string fieldName, MetaObject metaObject)
+        {
+            return metaObject.HasGetter(fieldName) && metaObject.HasSetter(fieldName);
+        }
+        public object getValueByName(string fieldName, MetaObject metaObject)
+        {
+            return metaObject.HasGetter(fieldName) ? metaObject.GetValue(fieldName) : null;
+        }
+
+    }
+}
