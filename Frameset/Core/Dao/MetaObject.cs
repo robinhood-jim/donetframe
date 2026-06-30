9b2f44a57bf0a5505af020c325226ce429ac48f8
robinjim@126.com <robinjim@126.com>
2026-06-30 19:20:00 +0800

    missing MetaObject

---
+++ Frameset/Core/Dao/MetaObject.cs
@@ -0,0 +1,39 @@
+﻿using Frameset.Core.Dao.Utils;
+using System.Collections.Generic;
+
+namespace Frameset.Core.Dao
+{
+    public class MetaObject
+    {
+        private readonly object originObj;
+        private readonly Dictionary<string, FieldContent> fieldDict;
+        public MetaObject(object originObj, Dictionary<string, FieldContent> fieldDict)
+        {
+            this.originObj = originObj;
+            this.fieldDict = fieldDict;
+        }
+        public object GetValue(string columnName)
+        {
+            if (fieldDict.TryGetValue(columnName, out FieldContent fieldContent))
+            {
+                return fieldContent.GetMethod.Invoke(originObj, null);
+            }
+            return null;
+        }
+        public void SetValue(string fieldName, object fieldValue)
+        {
+            if (fieldDict.TryGetValue(fieldName, out FieldContent fieldContent))
+            {
+                fieldContent.SetMethod.Invoke(originObj, [fieldValue]);
+            }
+        }
+        public bool HasGetter(string columnName)
+        {
+            return fieldDict.TryGetValue(columnName, out FieldContent fieldContent) && fieldContent.GetMethod != null;
+        }
+        public bool HasSetter(string columnName)
+        {
+            return fieldDict.TryGetValue(columnName, out FieldContent fieldContent) && fieldContent.SetMethod != null;
+        }
+    }
+}
